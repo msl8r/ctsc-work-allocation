d6b4cc048cdd38b7484d0e36e5c81b2baf2a9bcb
Attila Kiss <attila.kiss@hmcts.net>
2019-08-06 16:04:43 +0100

    [AK] setup shared vault, and suppress CVE-2019-14379 for now

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,6 +5,16 @@ locals {
   vaultName              = "${var.product}-${var.env}"
 }
 
+data "azurerm_key_vault" "workallocation_key_vault" {
+  name = "${local.vaultName}"
+  resource_group_name = "${var.product}-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "s2s_secret" {
+  name      = "CTSC-S2S-SECRET"
+  vault_uri = "${data.azurerm_key_vault.workallocation_key_vault.vault_uri}"
+}
+
 module "ctsc-work-allocation" {
   source              = "git@github.com:hmcts/cnp-module-webapp?ref=master"
   product             = "${var.product}-${var.component}"
@@ -18,10 +28,8 @@ module "ctsc-work-allocation" {
   app_settings = {
     LOGBACK_REQUIRE_ALERT_LEVEL = "false"
     LOGBACK_REQUIRE_ERROR_CODE  = "false"
+    S2S_SECRET = "${data.azurerm_key_vault_secret.s2s_secret.value}"
+    S2S_AUTH_URL = "http://${var.idam_s2s_url_prefix}-${var.env}.service.${local.ase_name}.internal"
   }
 }
 
-data "azurerm_key_vault" "payment_key_vault" {
-  name = "${local.vaultName}"
-  resource_group_name = "${var.product}-${var.env}"
-}
