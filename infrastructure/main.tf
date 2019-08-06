b12d952d04dd2af012ff90a2c54d5ed4fea42259
Attila Kiss <attila.kiss@hmcts.net>
2019-08-06 12:29:24 +0100

    [AK] creating vault for demo environment

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,20 +2,35 @@ provider "azurerm" {}
 
 locals {
   ase_name               = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  rg_name                = "ctsc-work-allocation-${var.env}-rg"
+  vault_rg_name          = "${local.rg_name}"
 }
 
-module "spring-boot-template" {
+module "ctsc-work-allocation" {
   source              = "git@github.com:hmcts/cnp-module-webapp?ref=master"
-  product             = "${var.product}-${var.component}"
+  product             = "${var.product}"
   location            = "${var.location_app}"
   env                 = "${var.env}"
   ilbIp               = "${var.ilbIp}"
   subscription        = "${var.subscription}"
   capacity            = "${var.capacity}"
   common_tags         = "${var.common_tags}"
+  asp_rg              = "${local.rg_name}"
 
   app_settings = {
     LOGBACK_REQUIRE_ALERT_LEVEL = "false"
     LOGBACK_REQUIRE_ERROR_CODE  = "false"
   }
+}
+
+module "key-vault" {
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  product                 = "${var.product}"
+  env                     = "${var.env}"
+  tenant_id               = "${var.tenant_id}"
+  object_id               = "${var.jenkins_AAD_objectId}"
+  resource_group_name     = "${local.vault_rg_name}"
+  # dcd_cc-dev group object ID
+  product_group_object_id = "${var.product_group_object_id}"
+  common_tags = "${var.common_tags}"
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -32,3 +32,15 @@ variable "idam_s2s_url_prefix" {
   default = "rpe-service-auth-provider"
 }
 
+variable "tenant_id" {}
+
+variable "jenkins_AAD_objectId" {
+  type                        = "string"
+  description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
+
+variable "product_group_object_id" {
+  description = "Product group (dcd_group_ctsc_v2) object ID"
+  default     = "075cc55b-c1d0-4fb9-a658-e5e0de0e13b1"
+}
+
