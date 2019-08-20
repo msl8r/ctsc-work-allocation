c634225fb5d5f77757b6ead05f76d3ef2b4d6fa2
Kiran Yenigala <kiran.yenigala@hmcts.net>
2019-08-20 14:42:33 +0100

    Fixed issues with commons collections

--- src/main/java/uk/gov/hmcts/reform/workallocation/config/AppConfig.java
+++ src/main/java/uk/gov/hmcts/reform/workallocation/config/AppConfig.java
@@ -3,11 +3,14 @@ package uk.gov.hmcts.reform.workallocation.config;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Properties;
 
 @Configuration
 @EnableScheduling
@@ -22,5 +25,16 @@ public class AppConfig {
             .setConnectionRequestTimeout(timeout * 1000)
             .setSocketTimeout(timeout * 1000).build();
         return HttpClientBuilder.create().setDefaultRequestConfig(config).build();
+    }
+
+    @Bean
+    public VelocityEngine getVelocityEngine() throws Exception {
+        Properties props = new Properties();
+        props.put("resource.loader", "class");
+        props.put("class.resource.loader.class",
+            "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+        VelocityEngine engine = new VelocityEngine(props);
+        engine.init();
+        return engine;
     }
 }
--- src/main/java/uk/gov/hmcts/reform/workallocation/services/EmailSendingService.java
+++ src/main/java/uk/gov/hmcts/reform/workallocation/services/EmailSendingService.java
@@ -1,35 +1,53 @@
 package uk.gov.hmcts.reform.workallocation.services;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.workallocation.model.Task;
 
 import java.io.StringWriter;
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 @Service
 @Slf4j
-public class EmailSendingService {
+public class EmailSendingService implements InitializingBean {
+
+    @Value("${smtp.host}")
+    private String smtpHost;
+
+    @Value("${smtp.port}")
+    private String smtpPort;
+
+    @Value("${smtp.user}")
+    private String smtpUser;
+
+    @Value("${smtp.password}")
+    private String smtpPassword;
+
+    @Value("${smtp.from}")
+    private String smtpFrom;
 
     @Value("${service.email}")
     private String serviceEmail;
 
-    @Value("${smtp.from}")
-    private String fromEmail;
-
     @Autowired
     private VelocityEngine velocityEngine;
 
-    @Autowired
-    private JavaMailSender javaMailSender;
-
-    private static final String ENCODING = "UTF-8";
+    private Session session;
 
     public void sendEmail(Task task, String deeplinkBaseUrl) throws Exception {
         log.info("Sending Email");
@@ -44,19 +62,49 @@ public class EmailSendingService {
             ? task.getJurisdiction().toLowerCase() + ".vm" :  "divorce.vm";
 
         StringWriter stringWriter = new StringWriter();
-        boolean templateMerged = velocityEngine
-            .mergeTemplate(templateFileName, ENCODING, velocityContext, stringWriter);
-
-        if (templateMerged) {
-            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
-            mimeMessageHelper.setTo(serviceEmail);
-            mimeMessageHelper.setFrom(fromEmail);
-            mimeMessageHelper.setSubject("Service: " + task.getJurisdiction() + ",State:" + task.getState());
-            mimeMessageHelper.setText(stringWriter.toString(), true);
-
-            javaMailSender.send(mimeMessage);
-            log.info("Email sending successful");
+        final Template template = velocityEngine.getTemplate("templates/" + templateFileName);
+        if (template == null) {
+            throw new RuntimeException("Template " + templateFileName + " not found ");
         }
+        template.merge(velocityContext, stringWriter);
+
+        MimeMessage msg = creatMimeMessage();
+        msg.setReplyTo(InternetAddress.parse(smtpFrom, false));
+        msg.setSubject("Service: " + task.getJurisdiction() + ",State:" + task.getState(), "UTF-8");
+        msg.setText(stringWriter.toString(), "UTF-8");
+        msg.setSentDate(new Date());
+
+        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(serviceEmail, false));
+        Transport.send(msg);
+        log.info("Email sending successful");
+    }
+
+    private MimeMessage creatMimeMessage() throws MessagingException {
+        MimeMessage msg = new MimeMessage(this.session);
+        msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+        msg.addHeader("format", "flowed");
+        msg.addHeader("Content-Transfer-Encoding", "8bit");
+        return msg;
+    }
+
+
+    private Session createSession() {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", smtpHost);
+        props.put("mail.smtp.port", smtpPort);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Authenticator auth = new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(smtpUser, smtpPassword);
+            }
+        };
+        return Session.getInstance(props, auth);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        this.session = createSession();
     }
 }
