12a1a8d343f5ec809fd069890c1cdaf1ee7f97a0
Kiran Yenigala <kiran.yenigala@hmcts.net>
2019-08-28 09:51:02 +0100

    Added refresh_token to Authorize

--- src/main/java/uk/gov/hmcts/reform/workallocation/idam/Authorize.java
+++ src/main/java/uk/gov/hmcts/reform/workallocation/idam/Authorize.java
@@ -12,12 +12,15 @@ public class Authorize {
     private String defaultUrl;
     private String code;
     private String accessToken;
+    private String refreshToken;
 
     public Authorize(@JsonProperty("default-url") String defaultUrl,
                      @JsonProperty("code") String code,
-                     @JsonProperty("access_token") String accessToken) {
+                     @JsonProperty("access_token") String accessToken,
+                     @JsonProperty("refresh_token") String refreshToken) {
         this.defaultUrl = defaultUrl;
         this.code = code;
         this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
     }
 }
