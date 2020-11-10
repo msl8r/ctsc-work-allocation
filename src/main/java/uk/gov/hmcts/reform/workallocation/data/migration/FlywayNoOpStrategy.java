f6b51356fab8956341dfe79fb78e1b80815e3b56
Rishi Sharma <rishi.sharma@hmcts.net>
2020-11-10 17:11:20 +0000

    DTSBPS-236: Bump flyway core 7.0.0

--- src/main/java/uk/gov/hmcts/reform/workallocation/data/migration/FlywayNoOpStrategy.java
+++ src/main/java/uk/gov/hmcts/reform/workallocation/data/migration/FlywayNoOpStrategy.java
@@ -16,5 +16,6 @@ public class FlywayNoOpStrategy implements FlywayMigrationStrategy {
             .ifPresent(info -> {
                 throw new PendingMigrationScriptException(info.getScript());
             });
+        flyway.migrate();
     }
 }
